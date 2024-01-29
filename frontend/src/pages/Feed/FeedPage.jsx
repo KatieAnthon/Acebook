dd52627b2f16047a1d05c44631c1fc955f9620e8
Katie <katieanthonisz@hotmail.co.uk>
2024-01-29 17:01:08 +0000

    updated database schema

--- frontend/src/pages/Feed/FeedPage.jsx
+++ frontend/src/pages/Feed/FeedPage.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { getPosts } from "../../services/posts";
+import { getPosts} from "../../services/posts";
 import Post from "../../components/Post/Post";
 
 export const FeedPage = () => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [post, setPost] = useState("")
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -29,9 +30,31 @@ export const FeedPage = () => {
     return;
   }
 
+  const handleSubmit = (event) => {
+    setPosts.push(e.target.post)
+    console.log("submission works")
+  }
+
   return (
     <>
       <h2>Posts</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Create Post:
+          <input
+          type="post"
+          name="post"
+          value={post}
+          onChange={(e) => setPost(e.target.post)}
+          />
+        </label>
+        <label>
+            Submit
+            <input type="submit" />
+            </label>
+      </form>
+
       <div className="feed" role="feed">
         {posts.map((post) => (
           <Post post={post} key={post._id} />
