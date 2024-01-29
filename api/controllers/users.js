4b28d5396560c29180d0be53a1d114dc245008bf
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-01-29 20:49:56 +0000

    latest changes

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const create = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const profilePicUrl = req.file ? req.file.path : ''; // Get the file path from Multer
+  const profilePicUrl = req.file ? req.file.path : ''; // Get the file path from Multer// multer is a lirary that we will need to add on our api end. 
   console.log("req.body:", req.body);  // Should log form fields other than file
   console.log("req.file:", req.file);  // Should log file information
 
