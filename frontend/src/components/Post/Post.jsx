1f55a3333f0ae960cc77da327330dc6d84279a40
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-01-30 16:13:29 +0000

    latest changes

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,6 +1,6 @@
 const Post = (props) => {
   if (!props.post || !props.post.user || !props.post.user.username) {
-    return null; // or display an appropriate loading or error message
+    return null; 
   }
   return (
     <article key={props.post._id}>
