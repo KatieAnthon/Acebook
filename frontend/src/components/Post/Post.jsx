df5afabe0d7039c9226e2948982353a6b114072d
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-01-30 16:05:20 +0000

    post now shows username

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,8 +1,11 @@
 const Post = (props) => {
+  if (!props.post || !props.post.user || !props.post.user.username) {
+    return null; // or display an appropriate loading or error message
+  }
   return (
-    <article key={props.post.id}>
+    <article key={props.post._id}>
       <div>
-        <p>Post created by {props.post.username}: {props.post.message}</p>
+        <p>Post created by {props.post.user.username}: {props.post.message}</p>
       </div>
     </article>
   );
