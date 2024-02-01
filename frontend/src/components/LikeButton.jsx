5334bdc14552b066e2a6d851b4724a0f81a347f9
ben-dh3 <benjaminduffield1997@gmail.com>
2024-02-01 17:11:07 +0000

    update

--- frontend/src/components/LikeButton.jsx
+++ frontend/src/components/LikeButton.jsx
@@ -1,40 +1,11 @@
 import { addUserLike } from "../services/posts"
 import { useState } from "react";
 import { getPosts } from "../services/posts";
-import { useEffect } from "react";
 
 const LikeButton = (likes) => {
 
     const [token, setToken] = useState(window.localStorage.getItem("token"));
     const [numberLikes, setNumberLikes] = useState(likes.likes.length)
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            if (token) {
-                try {
-                    const postsData = await getPosts(token);
-                    setNumberLikes(postsData.posts[0].likes.length);
-                } catch (err) {
-                    console.error('Error fetching posts:', err);
-                }
-            } else {
-                console.log('No token found, navigating to login.');
-            }
-        };
-        fetchData();
-    }, [token]);
-
-
-
-    // const handleAddLike = async () => {
-    //     try {
-    //         await addUserLike(token, { post_id: likes.post_id });
-    //         // Update the number of likes based on the updated length of likes array
-    //         setNumberLikes(likes.likes.length)
-    //     } catch (err) {
-    //         console.error("Error handling like", err.message);
-    //     }
-    // };
 
     const handleAddLike = async () => {
         try {
@@ -47,8 +18,6 @@ const LikeButton = (likes) => {
             console.error("Error handling like", err.message);
         }
     };
-
-
 
     return (
     <div>
