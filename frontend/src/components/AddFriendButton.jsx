b0afe410cacaafede6c915bfa364c6fb546270f6
ben-dh3 <benjaminduffield1997@gmail.com>
2024-02-05 09:53:32 +0000

    update

--- frontend/src/components/AddFriendButton.jsx
+++ frontend/src/components/AddFriendButton.jsx
@@ -1,2 +1,34 @@
 // find user_id from user profile
 // find user_id from token
+
+import { sendFriendRequest } from "../services/users"
+import { useState } from "react";
+import { getPosts } from "../services/posts";
+
+const AddFriendButton = () => {
+    const [token] = useState(window.localStorage.getItem("token"));
+    const [requestStatus, setRequestStatus] = useState('')
+
+    const handleSendFriendRequest = async () => {
+        try {
+            
+            await sendFriendRequest(token, { post_id: likes.post_id });
+            
+            const updatedPostsData = await getPosts(token);
+            const updatedPost = updatedPostsData.posts.find(post => post._id === likes.post_id);
+            // update the state request status
+            setRequestStatus('Friend Request Sent');
+        } catch (err) {
+            console.error("Error handling like", err.message);
+        }
+    };
+
+    return (
+    <div>
+        <button onClick={handleSendFriendRequest} >😁 Add Friend {requestStatus}
+        </button>
+    </div>
+    )
+}
+
+export default LikeButton;
---
+++ frontend/src/services/users.js
@@ -0,0 +1,44 @@
+// docs: https://vitejs.dev/guide/env-and-mode.html
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+
+export const sendFriendRequest = async (token, user_id) => {
+    const requestOptions = {
+    method: "POST",
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`,
+    },
+    body: JSON.stringify(user_id),
+};
+
+const response = await fetch(`${BACKEND_URL}/users/sendFriendRequest`, requestOptions);
+if (response.status !== 200) {
+    throw new Error("Unable to send friend request");
+}
+
+const data = await response.json();
+console.log("data", data)
+return data;
+};
+
+
+export const friendRequestResponse = async (token, user_id, confirmed) => {
+    const requestOptions = {
+    method: "POST",
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`,
+    },
+    body: JSON.stringify(user_id, confirmed),
+};
+
+const response = await fetch(`${BACKEND_URL}/users/friendRequestResponse`, requestOptions);
+if (response.status !== 200) {
+    throw new Error("Unable to respond to friend request");
+}
+
+const data = await response.json();
+console.log("data", data)
+return data;
+};
