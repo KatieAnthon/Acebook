5899b730924f19970f5284d894d96022d1cbc8a7
Katie <katieanthonisz@hotmail.co.uk>
2024-02-09 13:00:18 +0000

    Altered background image

--- frontend/src/pages/User/FriendProfile.jsx
+++ frontend/src/pages/User/FriendProfile.jsx
@@ -11,12 +11,15 @@ import './FriendProfile.css';
 import AboutMe from "../../components/UserInfo/AboutMe";
 import { Container, Card, Image } from "react-bootstrap";
 import banner from './banner.jpg';
+import Stack from 'react-bootstrap/Stack';
+import { MyMessages } from "../../pages/Message/MessagePage"
 
 export const FriendProfile = () => {
 const [posts, setPosts] = useState([]);
 const [token] = useState(window.localStorage.getItem("token"));
 const [FriendInfo, setFriendInfo] = useState([]);
 const { username } = useParams();
+const [comments, setComments] = useState([]);
 
 const navigate = useNavigate();
 
@@ -39,36 +42,52 @@ const navigate = useNavigate();
     fetchData();
   }, [token, username]);
 
+  const handleCommentSubmit = async (postId, commentText) => {
+    try {
+      const commentResponse = await addCommentToPost(token, postId, commentText);
+      const newComment = commentResponse.comment; 
+  
+      setPosts(currentPosts => currentPosts.map(post => {
+        if (post._id === postId) {
+          const comments = Array.isArray(post.comments) ? post.comments : [];
+          return { ...post, comments: [...comments, newComment] };
+        }
+        return post;
+      }));
+    } catch (err) {
+      console.error('Error adding comment:', err.message);
+    }
+  };
+
+  const openMessagesModal = (event) => {
+    event.preventDefault(); 
+    setIsModalOpen(true);   
+  };
+
   //{/* // style={{height: 500, width: '100%'}} src="../profile_cover_photo/friend_banner.jpg"  */}
 
   return (
   <div className="main-wrapper">
-    <div container-card>
-        <NavBar />
-          <Card>   
-            <Card.Img 
-              style={{ 
-              backgroundImage: `url(${banner})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              height: '500px'
-            }}/>
-              <Card.ImgOverlay>
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'  }}>
-              <div style={{ textAlign: "center", textDecorationColor: "color" }}>
-                  <Image variant="top" 
-                  style={{width: 100, height: 100, objectFit: 'cover', borderRadius: '50%'}}  
-                  src={FriendInfo.profilePic ? `http://localhost:3000/${FriendInfo.profilePic}` : 'default-picture-url'} />
-                  <Card.Body>
-                    <Card.Title style={{color: "white"}}>{username}</Card.Title>
-                    <Card.Text style={{color: "white"}}></Card.Text>
-                    <AddFriendButton user_id={FriendInfo.user_id} />
-                  </Card.Body>
+    <NavBar />
+        <Container fluid>
+          <Stack gap={3}>
+                {FriendInfo && (
+                  <div style={{ 
+                    backgroundImage: `url(${banner})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    height: '500px'// or whatever height you want
+                  }}>
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+                      <div style={{ textAlign: "center", color: "white" }}>
+                        <Image variant="top" style={{width: 100, height: 100, objectFit: 'cover', borderRadius: '50%'}} src={FriendInfo.profilePic ? `http://localhost:3000/${FriendInfo.profilePic}` : 'default-picture-url'} />
+                        <div>{FriendInfo.username || 'Default Username'}</div>
+                      </div>
+                    </div>
                   </div>
-                  </div>
-              </Card.ImgOverlay>
-          </Card>
-          </div>
+                )}
+            </Stack>
+        </Container>
           <Container fluid>
           <div className="row">
             <div className="col-lg-6">
@@ -78,21 +97,28 @@ const navigate = useNavigate();
                     </div>
                   </div>
                   </div>
+                 
                     
                     
             <div className="col-lg-6">
             <div className="right-column">
           <div className="container-posts">
         {posts.map((post) => (
-          <Post key={post._id} post={post} />
+          <Post 
+          key={post._id} 
+          post={post}
+          onCommentSubmit={handleCommentSubmit}
+          focusCommentForm={() => focusCommentForm(post._id)} 
+          />
         ))}
-        
         </div>
         </div>
         </div>
         </div>
         </Container>
         </div>
+        
+    
 );
 };
 
